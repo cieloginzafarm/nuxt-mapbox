dffa7dbab3508860c8f4bafb444efa8a1a7d3d1c
cieloginzafarm <cielo@ginzafarm.co.jp>
2020-10-09 11:44:46 +0900

    Did some formatting.

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,78 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        nuxt-mapbox
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <script src="https://api.tiles.mapbox.com/mapbox.js/plugins/turf/v3.0.11/turf.min.js"></script>
+    <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.2.0/mapbox-gl-draw.js"></script>
+    <link
+      rel="stylesheet"
+      href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.2.0/mapbox-gl-draw.css"
+      type="text/css"
+    />
+    <div id="map" @click="getCoordinates"></div>
+    <div id="calculated-area"></div>
   </div>
 </template>
 
 <script>
-export default {}
+import mapboxgl from "mapbox-gl";
+export default {
+  data() {
+    return {
+      map: {},
+      coordinates: [],
+      marker: null,
+    };
+  },
+  mounted() {
+    const mapboxgl = require("mapbox-gl");
+    this.createMap();
+    this.getCoordinates();
+  },
+  methods: {
+    createMap() {
+      mapboxgl.accessToken = this.access_token;
+      this.map = new mapboxgl.Map({
+        // accessToken: process.env.MAPBOX_TOKEN,
+        accessToken:
+          "pk.eyJ1IjoiY2llbG9naW56YWZhcm0iLCJhIjoiY2tnMGczbjIyMGQwMTJybGI4MGJsNTBzbSJ9.pPLT9Q2Kj_dUVW7BAqAyHw",
+        container: "map",
+        // style: process.env.MAPBOX_STYLE,
+        style: "mapbox://styles/cieloginzafarm/ckg0gyibf04ld1aqds8wen9jh",
+        center: [139.684363, 35.794831],
+        zoom: 17,
+      });
+
+      this.draw = new MapboxDraw({
+        displayControlsDefault: false,
+        controls: {
+          polygon: true,
+          trash: true,
+        },
+      });
+      this.map.addControl(this.draw);
+    },
+    getCoordinates() {
+      self = this;
+      this.map.on("click", function (e) {
+        const currPos = JSON.parse(JSON.stringify(e.lngLat.wrap()));
+        self.coordinates = [currPos["lng"], currPos["lat"]];
+        if (self.marker != null) {
+          self.marker.remove()
+        }
+        self.setCurrentMarker();
+      });
+    },
+    setCurrentMarker() {
+      this.marker = new mapboxgl.Marker()
+        .setLngLat(this.coordinates)
+        .addTo(this.map);
+    },
+  },
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style scoped>
+#map {
+  width: 100%;
+  height: 100vh;
 }
 </style>
